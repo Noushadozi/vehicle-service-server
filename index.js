121653858cb31444a36cf820c7b54ab7f55b2072
Noushadozi <noushadozi333@gmail.com>
2023-11-15 13:55:50 +0600

    bookings added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["https://cars-hospital.web.app", "http://localhost:5173"],
+    origin: ["https://cars-hospital.web.app", "http://localhost:5173","http://localhost:5174"],
     credentials: true,
   })
 );
@@ -34,7 +34,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-   
+    const serviceCollection = client.db("car-doctor").collection("services");
+    const bookingCollection = client.db("car-doctor").collection("bookings");
+
+
+    // bookings
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      if (req.user.email !== req.query.email) {
+        return res.send({ message: "forbidden access" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
