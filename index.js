3609b9cb41b39cc413d672fd6a2ed920c58ce406
Noushadozi <noushadozi333@gmail.com>
2023-12-01 12:24:54 +0600

    jwt

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["https://cars-hospital.web.app", "http://localhost:5173","http://localhost:5174"],
+    origin: [
+      "https://cars-hospital.web.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
     credentials: true,
   })
 );
@@ -27,7 +31,24 @@ const client = new MongoClient(uri, {
   },
 });
 
+const logger = (req, res, next) => {
+  console.log("login:info", req.method, req.url);
+  next();
+};
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -37,6 +58,49 @@ async function run() {
     const serviceCollection = client.db("car-doctor").collection("services");
     const bookingCollection = client.db("car-doctor").collection("bookings");
 
+    //! auth related api
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ token });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
+    //! services related api
+    app.get("/services", async (req, res) => {
+      const filter = req.query;
+      const query = { title: { $regex: filter.search, $options: "i" } };
+      const options = {
+        sort: {
+          price: filter.sort === "asc" ? 1 : -1,
+        },
+      };
+      const cursor = serviceCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
     // bookings
     app.get("/bookings", logger, verifyToken, async (req, res) => {
